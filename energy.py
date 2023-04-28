49b730f36069c3d6e769c2dc434f5e0be11c5643
PavanNikeathan <132044211+PavanNikeathan@users.noreply.github.com>
2023-04-28 14:23:57 +0530

    Update energy.py

--- energy.py
+++ energy.py
@@ -1,18 +1,17 @@
-# Import libraries
+
 import streamlit as st
-import torch
+
 from transformers import pipeline
 
-# Set page title
+
 st.set_page_config(page_title='Energy Consumption Chatbot', page_icon=':zap:')
 
-# Load chatbot model
+
 model = pipeline('text2text-generation', model='GPT2')
 
-# Set welcome message
 st.write('Welcome to the Energy Consumption Chatbot! How can I assist you today?')
 
-# Chatbot function
+
 def run_chatbot():
     user_input = st.text_input('You: ')
 
@@ -20,5 +19,5 @@ def run_chatbot():
         chatbot_response = model(user_input)[0]['generated_text']
         st.write('Chatbot: ' + chatbot_response)
 
-# Run chatbot
+
 run_chatbot()
