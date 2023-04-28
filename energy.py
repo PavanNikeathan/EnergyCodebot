d334edc4627285b9d29e81ca8719bb5311cf041f
PavanNikeathan <132044211+PavanNikeathan@users.noreply.github.com>
2023-04-28 14:34:33 +0530

    Update energy.py

--- energy.py
+++ energy.py
@@ -1,33 +1,31 @@
 import streamlit as st
 
 # Set up greeting message
-st.sidebar.title("Water Conservation Chatbot")
+st.sidebar.title("Energy Conseumption Chatbot")
 st.sidebar.write("Enter a message to start a conversation with the chatbot")
 
 # Define conversation logic
-def water_chatbot(msg):
+def Energy_chatbot(msg):
     # Convert input message to lowercase
     msg = msg.lower()
 
-    # Define water conservation-related keywords
-    conservation_keywords = ["water", "conservation", "save", "usage", "waste"]
-    shower_keywords = ["shower", "bath", "faucet"]
-    irrigation_keywords = ["irrigation", "lawn", "garden", "plants"]
-    consumption_keywords = ["meter", "bill", "monitor"]
+# Define conservation-related keywords
+energy_keywords = ["energy", "conservation", "save", "usage", "waste"]
+appliance_keywords = ["appliances", "electronics", "devices", "power", "usage"]
+lighting_keywords = ["lighting", "bulbs", "led", "efficiency"]
 
-    # Check for relevant keywords in message and respond accordingly
-    if any(word in msg for word in conservation_keywords):
-        response = "Here are some water conservation tips: \n\n- Fix leaky faucets \n- Take shorter showers \n- Water your lawn in the morning or evening \n- Install a low-flow toilet"
-    elif any(word in msg for word in shower_keywords):
-        response = "Taking shorter showers and turning off the water while you soap up can save a lot of water over time."
-    elif any(word in msg for word in irrigation_keywords):
-        response = "Consider planting drought-resistant plants or installing a drip irrigation system to conserve water in your garden."
-    elif any(word in msg for word in consumption_keywords):
-        response = "To monitor your water consumption, you can check your meter or review your water bill. You can also install a smart water meter to track your usage in real-time."
-    else:
-        response = "I'm sorry, I'm not sure how to help with that. Please ask me about water conservation!"
+# Check for relevant keywords in message and respond accordingly
+if any(word in msg for word in energy_keywords):
+    response = "Here are some energy conservation tips: \n\n- Use energy-efficient appliances and electronics \n- Turn off appliances and electronics when not in use \n- Use power strips to reduce standby power usage \n- Keep your home well-insulated"
+elif any(word in msg for word in appliance_keywords):
+    response = "Using energy-efficient appliances and electronics, and turning them off when not in use, can help reduce your energy usage."
+elif any(word in msg for word in lighting_keywords):
+    response = "Switching to LED light bulbs and turning off lights when you leave a room can save a lot of energy over time."
+else:
+    response = "I'm sorry, I'm not sure how to help with that. Please ask me about energy conservation!"
 
-    return response
+return response
+
 
 # Set up conversation interface
 user_input = st.text_input("You:", "")
